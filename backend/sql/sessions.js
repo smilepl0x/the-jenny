1bccb163194b68541240045810cf936d3895fea7
smilepl0x <47682247+smilepl0x@users.noreply.github.com>
2023-12-26 23:03:51 -0600

    fix table join

--- backend/sql/sessions.js
+++ backend/sql/sessions.js
@@ -3,5 +3,5 @@ export const SESSIONS = {
   REMOVE_SESSION: `DELETE FROM sessions WHERE message_id=?`,
   JOIN_SESSION: `UPDATE sessions SET party_members=JSON_ARRAY_APPEND(party_members, "$", ?) where message_id=? AND NOT JSON_CONTAINS(party_members, JSON_QUOTE(?), "$")`,
   LEAVE_SESSION: `UPDATE sessions SET party_members=JSON_REMOVE(party_members, JSON_UNQUOTE(JSON_SEARCH(party_members, 'one', ?))) where message_id=? AND JSON_CONTAINS(party_members, JSON_QUOTE(?), "$")`,
-  GET_SESSION_WITH_GAME: `SELECT * FROM sessions INNER JOIN games on games.game_id WHERE message_id=?`,
+  GET_SESSION_WITH_GAME: `SELECT * FROM sessions s LEFT JOIN games g ON g.game_id = s.game_id WHERE message_id=?`,
 };
